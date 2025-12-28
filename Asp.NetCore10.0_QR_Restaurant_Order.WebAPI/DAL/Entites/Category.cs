b935495d2a621ede87887ec1f6d1d4ba76819c83
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-28 22:13:39 +0300

    3# Product + Category Entites Added

---
+++ Asp.NetCore10.0_QR_Restaurant_Order.WebAPI/DAL/Entites/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Asp.NetCore10._0_QR_Restaurant_Order.WebAPI.DAL.Entites
+{
+    public class Category
+    {
+        public int CategoryID { get; set; } // Kategori ID
+        public string CategoryName { get; set; } // Kategori Adı
+        public bool CategoryStatus { get; set; } // Kategori Durumu (Aktif/Pasif)
+    }
+}
---
+++ Asp.NetCore10.0_QR_Restaurant_Order.WebAPI/DAL/Entites/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Asp.NetCore10._0_QR_Restaurant_Order.WebAPI.DAL.Entites
+{
+    public class Product
+    {
+        public int ProductID { get; set; } // Ürün ID
+        public string ProductName { get; set; } // Ürün Adı
+        public string ProductDescription { get; set; } // Ürün Açıklaması
+        public decimal ProductPrice { get; set; } //Ürün Fiyatı
+        public string ProductImageURL { get; set; } // Ürünün Görseli
+        public bool ProductStatus { get; set; } // Ürün Durumu (Aktif/Pasif)
+    }
+}
