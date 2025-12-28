9d37077a224d88dab1b06af537a16a6120991856
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-28 22:09:17 +0300

    Asp.NetCore10.0_QR_Restaurant_Order.WebUI Process Added

---
+++ Asp.NetCore10.0_QR_Restaurant_Order.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.NetCore10._0_QR_Restaurant_Order.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
