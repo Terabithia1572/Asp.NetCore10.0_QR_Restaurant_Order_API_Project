d5bfaaecca5b2fe0ba57456d6cbd532969dafe40
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-29 20:45:58 +0300

    6# Business + DataAccess + Entity + DTO Layers Added

---
+++ Asp.NetCore10.0_QR_Restaurant_Order.DataAccessLayer/Concrete/SignalRContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asp.NetCore10._0_QR_Restaurant_Order.DataAccessLayer.Concrete
+{
+    public class SignalRContext
+    {
+    }
+}
