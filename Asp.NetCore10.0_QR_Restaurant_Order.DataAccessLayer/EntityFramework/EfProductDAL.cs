f854db1c981a070c5089185e7101ca17ee2e22d6
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-29 21:24:01 +0300

    9# Completing DataAccessLayer + EntityFramework + Repositories Added

---
+++ Asp.NetCore10.0_QR_Restaurant_Order.DataAccessLayer/EntityFramework/EfProductDAL.cs
@@ -0,0 +1,17 @@
+﻿using Asp.NetCore10._0_QR_Restaurant_Order.DataAccessLayer.Abstract;
+using Asp.NetCore10._0_QR_Restaurant_Order.DataAccessLayer.Concrete;
+using Asp.NetCore10._0_QR_Restaurant_Order.DataAccessLayer.Repositories;
+using Asp.NetCore10._0_QR_Restaurant_Order.EntityLayer.Entites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asp.NetCore10._0_QR_Restaurant_Order.DataAccessLayer.EntityFramework
+{
+    public class EfProductDAL : GenericRepository<Product>, IProductDAL
+    {
+        public EfProductDAL(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Asp.NetCore10.0_QR_Restaurant_Order.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Asp.NetCore10._0_QR_Restaurant_Order.DataAccessLayer.Abstract;
+using Asp.NetCore10._0_QR_Restaurant_Order.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asp.NetCore10._0_QR_Restaurant_Order.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
+    {
+        private readonly SignalRContext _context;
+
+        public GenericRepository(SignalRContext context)
+        {
+            _context = context;
+        }
+
+        public void TAdd(T t)
+        {
+            _context.Set<T>().Add(t); // Generic olarak gelen T tipindeki varlığı ekle
+            _context.SaveChanges(); // Değişiklikleri veritabanına kaydet
+        }
+
+        public void TDelete(T t)
+        {
+            _context.Remove(t); // Generic olarak gelen T tipindeki varlığı sil
+            _context.SaveChanges(); // Değişiklikleri veritabanına kaydet
+        }
+
+        public T TGetByID(int id)
+        {
+            return _context.Set<T>().Find(id); // Generic olarak gelen T tipindeki varlığı ID ile bul ve döndür
+        }
+
+        public List<T> TGetListAll()
+        {
+           return _context.Set<T>().ToList(); // Generic olarak gelen T tipindeki tüm varlıkları listele ve döndür
+        }
+
+        public void TUpdate(T t)
+        {
+            _context.Update(t); // Generic olarak gelen T tipindeki varlığı güncelle
+            _context.SaveChanges(); // Değişiklikleri veritabanına kaydet
+        }
+    }
+}
