3fbe20828fa52f370724a42952be803ba0b3a608
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2026-01-01 15:23:46 +0300

    27# AdminLayout Process Added

---
+++ Asp.NetCore10.0_QR_Restaurant_Order.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_QR_Restaurant_Order.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
