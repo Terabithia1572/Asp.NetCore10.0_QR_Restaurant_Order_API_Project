cb369ee0371a6d1bd8e31725c0bf9eb9136e4582
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2026-01-01 14:43:33 +0300

    26# The process of retrieving category names from AutoMapper has been added.

--- Asp.NetCore10.0_QR_Restaurant_Order.WebAPI/Mapping/ProductMap/ProductMapping.cs
+++ Asp.NetCore10.0_QR_Restaurant_Order.WebAPI/Mapping/ProductMap/ProductMapping.cs
@@ -12,7 +12,9 @@ namespace Asp.NetCore10._0_QR_Restaurant_Order.WebAPI.Mapping.ProductMap
             CreateMap<Product, CreateProductDTO>().ReverseMap(); //Product ile CreateProductDTO arasında çift yönlü eşleme yapar
             CreateMap<Product, UpdateProductDTO>().ReverseMap(); //Product ile UpdateProductDTO arasında çift yönlü eşleme yapar
             CreateMap<Product, GetProductByIDDTO>().ReverseMap(); //Product ile GetProductByIDDTO arasında çift yönlü eşleme yapar
-            CreateMap<Product, ResultProductWithCategoryDTO>().ReverseMap(); //Product ile ResultProductWithCategoryDTO arasında çift yönlü eşleme yapar    
+            CreateMap<Product, ResultProductWithCategoryDTO>()
+    .ForMember(dest => dest.CategoryName,
+        opt => opt.MapFrom(src => src.Category != null ? src.Category.CategoryName : "")); // Product ile ResultProductWithCategoryDTO arasında eşleme yapar ve CategoryName alanını doldurur  
         }
     }
 }
