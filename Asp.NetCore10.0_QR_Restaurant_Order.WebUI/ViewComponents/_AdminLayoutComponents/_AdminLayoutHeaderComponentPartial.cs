4aa8ad8486d0882c46237c3e01d76d35a2f9ec63
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2026-01-03 14:43:42 +0300

    28# 28# Added documentation comments for admin layout header and preloader components

---
+++ Asp.NetCore10.0_QR_Restaurant_Order.WebUI/ViewComponents/_AdminLayoutComponents/_AdminLayoutHeaderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_QR_Restaurant_Order.WebUI.ViewComponents._AdminLayoutComponents
+{
+    public class _AdminLayoutHeaderComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
