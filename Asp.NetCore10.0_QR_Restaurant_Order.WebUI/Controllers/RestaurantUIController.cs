c4252c1fdeb635d9705811081e9eaa24ef2533fe
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2026-01-17 23:43:57 +0300

    UI Process Added

---
+++ Asp.NetCore10.0_QR_Restaurant_Order.WebUI/Controllers/RestaurantUIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_QR_Restaurant_Order.WebUI.Controllers
+{
+    public class RestaurantUIController : Controller
+    {
+        public IActionResult _QRRestaurantLayout()
+        {
+            return View();
+        }
+    }
+}
