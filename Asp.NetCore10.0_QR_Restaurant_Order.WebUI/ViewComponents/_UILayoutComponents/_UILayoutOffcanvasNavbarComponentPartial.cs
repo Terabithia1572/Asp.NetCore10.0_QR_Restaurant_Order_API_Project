3e67d67e4eae43f39681c4231e69a37fb8e2a26f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2026-01-23 20:41:33 +0300

    Refactor UI Layout: Extract Head, Navbar, Offcanvas, Hero and HowItWorks into components

---
+++ Asp.NetCore10.0_QR_Restaurant_Order.WebUI/ViewComponents/_UILayoutComponents/_UILayoutOffcanvasNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_QR_Restaurant_Order.WebUI.ViewComponents._UILayoutComponents
+{
+    public class _UILayoutOffcanvasNavbarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
